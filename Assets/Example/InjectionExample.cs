6402dacfbd32d29b1b3989c35e5e5befb822dc8a
Hendrik Demmer <hendrik.demmer@wooga.net>
2017-12-20 11:27:12 +0100

    Add a very basic example

---
+++ Assets/Example/InjectionExample.cs
@@ -0,0 +1,46 @@
+﻿using Injection;
+using UnityEngine;
+
+namespace Example
+{
+	public class InjectionExample : MonoBehaviour
+	{
+		private Injector _injector;
+
+		public class InjectableA : IInjectable
+		{
+			public readonly string HelloMessage = "Hello from A";
+		}
+
+		public class InjectableB : IInjectable
+		{
+			public readonly string GoodbyeMessage = "Goodbye from B";
+		}
+
+		public class Injected
+		{
+			[Inject] protected InjectableA A;
+			[Inject] protected InjectableB B;
+
+			public void PrintMessages()
+			{
+				Debug.Log(A.HelloMessage);
+				Debug.Log(B.GoodbyeMessage);
+			}
+		}
+
+		protected void Start ()
+		{
+			_injector = new Injector();
+		
+			_injector.Bind<InjectableA>(new InjectableA());
+			_injector.Bind<InjectableB>(new InjectableB());
+			
+			var injected = new Injected();
+			_injector.Inject(injected);
+
+			injected.PrintMessages();
+		}
+	
+	}
+}
